c3642be1d2173df92b447f18e02bd951617f664c
amramy <amr_amy@hotmail.com>
2022-11-20 11:43:09 -0500

    Challenge sql data and queries with images

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,156 @@
+--DELIVERABLE 1: The Number of Retiring Employees by Title 
+
+-- Query 
+-- Create table Retirement_Titles with EE info, birthdate, title
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+-- INNER JOIN on primary key
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+-- filter on birthdate
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+-- ORDER BY employee number
+ORDER BY emp_no 
+
+-- view retirement_titles
+SELECT * FROM retirement_titles;
+
+-- EXPORT retirement_titles as to data folder as csv. 
+
+--Query 
+--REMOVE duplicates from retirement_titles
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+--view table 
+SELECT * FROM unique_titles;
+
+-- EXPORT unique_titles to data folder as csv.
+
+-- Query 
+-- COUNT # of EE recent job title about to retire
+SELECT COUNT (title) 
+FROM unique_titles;
+-- output 72458 total titles
+
+-- CREATE "Retiring Titles" table with # EE for each title
+-- NotiCE "AS count" created new column "count", title column as title
+SELECT COUNT(emp_no) AS count, title 
+INTO retiring_titles --(count, title)
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+-- view retiring_titles table
+SELECT * FROM retiring_titles;
+
+-- EXPORT "retiring_titles" to data folder as csv.
+
+
+--Deliverable 2: The Employees Eligible for the Mentorship Program
+-- CREATE "Mentorship Eligibilty" file with current EE's born in 1965
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibilty;
+
+-- EXPORT mentorship_eligibilty to DAta file as csv.
+
+--DELIVERABLE #3: Two additional queries to provide more insight into the upcoming "silver tsunami."
+--length of employment
+-- Avg length of employment
+-- AVG lenght of employment per department
+
+-- FIND LENGTH OF EMPLOYMENT
+
+--List of non_current employees = 118660
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.hire_date,
+	de.from_date,
+	de.to_date,
+	t.title
+--INTO non_current_employees
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE de.to_date NOT IN ('9999-01-01')
+ORDER BY to_date DESC;
+
+SELECT * FROM non_current_employees;
+-- this data shows me comparisson with "hire_date" and "from_date". 
+-- realizing I don't need "from_date" with job transphers
+-- I also don't need titles
+
+DROP TABLE non_current_employees
+
+-- recreate non_current_employees = 91479 rows
+-- NOTE this did not have duplicate EE's... NICE. 
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.hire_date,
+	de.to_date
+INTO non_current_employees
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+WHERE de.to_date NOT IN ('9999-01-01')
+ORDER BY to_date DESC;
+
+-- ADD another column with durration of employment
+SELECT emp_no,
+	first_name,
+	last_name,
+	hire_date,
+	to_date,
+	to_date - hire_date AS durration_of_employment
+INTO non_ee_durration
+FROM non_current_employees
+ORDER BY durration_of_employment DESC;
+
+--View table 
+SELECT * FROM non_ee_durration;
+-- EXPORT to data file as csv
+
+--find avereage length of employment from non_current_employees... in DAYS.
+-- output... findings = 2,635.32 days
+SELECT ROUND(AVG(durration_of_employment), 2) AS "Average durration of Employees no longer with PH (DAYS)"
+INTO avg_durration_non_current_ee
+FROM non_ee_durration;
+
+-- view results
+SELECT * FROM avg_durration_non_current_ee
+-- EXPORT to data file as csv
+
+-- COMMIT TO GIT HUB
--- schema.sql
+++ schema.sql
@@ -7,10 +7,10 @@ CREATE TABLE departments (
 	UNIQUE (dept_name)
 );
 
-DROP TABLE employees CASCADE;
+DROP TABLE employees;
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
-	bith_date DATE NOT NULL,
+	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL, 
@@ -130,3 +130,197 @@ SELECT * FROM retirement_info;
 -- enter file path, format CSV, header toggle "YES", comma delimiter
 -- Commit to GIT HUB
 
+-- We need to add EE id number
+--Drop table so we can recreate
+DROP TABLE retirement_info;
+
+-- Add "emp_no" to our table
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- shOW THE new table
+SELECT * FROM retirement_info;
+
+-- LETS practice JOIN 
+
+--SELECT table.column_name, (selecting columns we want to view from each table)
+	--table.column_name,
+--FROM table (points to first table to be joined)
+--JOIN TYPE (points to the second table to be joined)
+--ON (indicates where Postgres should look for matches);
+
+--  INNER Join departments and dept_manager tables
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+--  INNER JOIN WITH ALIAS
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- LEFT Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- let's clean up our code... recreate above code
+-- use AS to create nicknames
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no;
+
+--create NEW TABLE AS current_emp
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01')
+
+
+SELECT * FROM retirement_info;
+
+-- COUNT & Group By(has no order, if re-run different order)
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+SELECT * FROM departments;
+-- ORDER BY
+-- Employee count by department number
+-- ** NOTICE order of dept_no is acending
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- CREATE NEW TABLE WITH ABOVE DATA ** add INTO 
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO department_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+SELECT * FROM department_count
+
+-- export department_count table 
+
+-- lets look at the salaries table
+SELECT * FROM salaries;
+
+-- Decending order of to_date column
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- The dates look wierd... not most recent... must be something with salary
+-- New table from employees, salaries and dept_emp
+-- first select columns from each table
+SELECT e.emp_no,
+e.first_name,
+e.last_name, 
+e.gender, 
+s.salary, 
+de.to_date
+INTO emp_info
+-- call out alias employees table AS e, salaries AS s, 
+FROM employees as e
+-- INNER JOIN 
+INNER JOIN salaries AS s
+ON (e.emp_no = s.emp_no)
+-- ADD third table to join and alias AS de
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+-- ADD filter ... range of dates to look at
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ -- ADD filter in the WHERE clause ... to_date from dept_emp using "AND"
+ 	AND (de.to_date = '9999-01-01');
+ 
+ -- I miss spelled birth_date in employee table creating... RENAME!
+ALTER TABLE employees RENAME COLUMN bith_date TO birth_date;
+
+SELECT * FROM emp_info;
+
+-- Create new table of Managers who will retire soon(ish)
+-- List of managers per department
+SELECT dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- create query returning only retiring Sales Team info
+SELECT * FROM dept_info;
+
+SELECT * FROM retirement_info;
+
+-- output 5860 (department Number d007)
+SELECT *
+INTO sales_retire_info
+FROM dept_info
+WHERE dept_name = 'Sales';
+
+--output 9281 (department Number d005)
+SELECT *
+FROM dept_info
+WHERE dept_name = 'Development';
+
+
+-- output 15141 
+SELECT *
+INTO sales_development_retire_info
+FROM dept_info
+WHERE dept_name = 'Sales'
+OR dept_name = 'Development';
+
+SELECT * FROM sales_development_retire_info;
