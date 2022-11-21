61481f57efac4781d67f204f4cc6b29eb0b99a31
amramy <amr_amy@hotmail.com>
2022-11-21 11:58:24 -0500

    updated

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -79,7 +79,7 @@ ON e.emp_no = t.emp_no
 WHERE de.to_date = '9999-01-01'
 AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 ORDER BY emp_no;
-
+ 
 SELECT * FROM mentorship_eligibilty;
 
 -- EXPORT mentorship_eligibilty to DAta file as csv.
